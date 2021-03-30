0a1daeb337aca629c534fce826eaa54415c9ea1f
Anthony Morris <amorriscode@gmail.com>
2021-03-30 15:31:04 -0700

    Convert project to use tsdx

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import { AppConfig, UserSession, showConnect } from '@stacks/connect';
+
+export default function init(walletAddress: string) {
+  const appConfig = new AppConfig(['store_write', 'publish_data']);
+  const userSession = new UserSession({ appConfig });
+
+  function authenticate() {
+    showConnect({
+      appDetails: {
+        name: 'My App',
+        icon: window.location.origin + '/my-app-logo.svg',
+      },
+      redirectTo: '/',
+      finished: () => {
+        let userData = userSession.loadUserData();
+        console.log(userData);
+      },
+      userSession: userSession,
+    });
+  }
+  console.log(walletAddress);
+  authenticate();
+}
